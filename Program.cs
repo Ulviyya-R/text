ad10c4bad3d02095f2fab06215d452e798231a79
Ulviyya M. Rustamova <ulviyyamr@code.local>
2022-11-15 21:28:33 +0400

    this is text

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int natural = 0;
+int cavab = default;
+
+
+for (int i = 1; i <= natural; i*=10)
+{
+    cavab = i;
+}
+if (cavab == 0)
+{
+    int b = 3 * 10 + natural;
+    System.Console.WriteLine(b);
+    return;
+}
+cavab *= 10;
+int a = 3 * cavab + natural;
+System.Console.WriteLine(a);
+
